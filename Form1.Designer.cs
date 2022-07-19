70bb68dbc6d6eb171a153551664fe55394bbb337
Geerome <33005569+Geeerome@users.noreply.github.com>
2022-07-19 19:36:08 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ParkCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.dateTimePicker3);
+            this.panel1.Controls.Add(this.dateTimePicker4);
+            this.panel1.Controls.Add(this.dateTimePicker2);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(32, 31);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1046, 709);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(709, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 52);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Time Out:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 127);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 52);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Time In:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(853, 457);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 51);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker3.Location = new System.Drawing.Point(775, 292);
+            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.ShowUpDown = true;
+            this.dateTimePicker3.Size = new System.Drawing.Size(144, 27);
+            this.dateTimePicker3.TabIndex = 8;
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker4.Location = new System.Drawing.Point(235, 292);
+            this.dateTimePicker4.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.Size = new System.Drawing.Size(144, 27);
+            this.dateTimePicker4.TabIndex = 7;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(724, 226);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(249, 27);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(184, 226);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(249, 27);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(315, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(658, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DateTimeLbl";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1115, 770);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parkulator";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+
+namespace ParkCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        DateTime currentDateTime = DateTime.Now;
+
+        public void Calc() {
+
+            //setting the proper format for each date and time
+            dateTimePicker1.CustomFormat = ("MM/dd/yyyy HH");
+            dateTimePicker2.CustomFormat = ("MM/dd/yyyy HH");
+            dateTimePicker3.CustomFormat = "HH:MM:SS";
+            dateTimePicker4.CustomFormat = "HH:MM:SS";
+
+            //time for Time in section
+            DateTime TimeInDate = (DateTime)dateTimePicker1.Value;
+
+            //time for Time out section
+            DateTime TimeOutDate = (DateTime)dateTimePicker2.Value;
+
+     
+
+            TimeSpan ts = TimeOutDate.Subtract(TimeInDate); // subtracting time in to time out to get the duration in hours
+
+
+            
+            string Days = ts.Days.ToString();
+            int hrs = (Convert.ToInt32(Days));
+            string Gp = "03:00:00";
+            TimeSpan GracePeriod = TimeSpan.Parse(Gp);
+            TimeSpan ts6 = dateTimePicker3.Value.Subtract(dateTimePicker4.Value);
+            Double DURATION = ((TimeOutDate - TimeInDate) + ts6 - GracePeriod).TotalHours; //Duration with grace period deducted
+
+            Double DURATIONS = ((TimeOutDate - TimeInDate) + ts6).TotalHours; //Total Duration
+
+
+            
+
+            int GracePayment = 50;
+            int sccdhrs = 10;
+            double VAT = .12;
+            double newDur2 = Math.Round(DURATIONS);
+
+
+            string timein = TimeInDate.ToString("MM/dd/yyyy") + " " + dateTimePicker4.Value.ToString("HH:mm:ss");
+            string timeout = TimeOutDate.ToString("MM/dd/yyyy") + " " + dateTimePicker3.Value.ToString("HH:mm:ss");
+
+
+
+            if (DURATION <= 0)
+            {
+
+                double  totalPayment = (GracePayment * VAT) + GracePayment;
+
+               
+
+                MessageBox.Show(" UNDERGRACE PERIOD");
+                MessageBox.Show("TIME IN :" + timein +
+                    "\n TIME OUT: " + timeout +
+                     "\n DURATION: " + newDur2 + " Hours"
+                    + "\n VAT SALE: " + totalPayment
+                    + "\nVAT AMOUNT: " + (GracePayment * VAT)
+                    + "\n TOTAL AMOUNT TO BE PAID: " + totalPayment
+                    );
+
+            } 
+
+            else {
+
+                int res = Convert.ToInt32(DURATION);
+
+                double overGraced = (res * sccdhrs);
+                double lastP = (overGraced + GracePayment);
+                double VATSale = (lastP * VAT);
+                double totalPayment2 = (lastP * VAT) + lastP;
+
+                MessageBox.Show(" OVERGRACE PERIOD  ");
+
+                MessageBox.Show("TIME IN :" + timein +
+                    "\n TIME OUT: " + timeout +
+                     "\n DURATION: " + newDur2 + " Hours"
+                    + "\n VAT SALE: " + VATSale
+                    + "\nVAT AMOUNT: " + lastP
+                    + "\n TOTAL AMOUNT TO BE PAID: " + totalPayment2
+                    );
+  
+                
+            
+            }
+  
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("dddd,MM/dd/yyyy HH:mm:ss");
+
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Calc();
+        }
+    }
+}
